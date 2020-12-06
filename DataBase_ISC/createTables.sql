62d8d8bd1f5aa398294c760348c8c06472fb4eb4
Alberto <albemene97@gmail.com>
2020-12-06 16:45:00 +0100

    Change name

--- DataBase_ISC/createTables.sql
+++ DataBase_ISC/createTables.sql
@@ -1,4 +1,42 @@
-CREATE TABLE IF NOT EXISTS bms1 (
+CREATE TABLE IF NOT EXISTS 'bms1' (
+    'date' DATE NOT NULL,
+    'time' TIME NOT NULL,
+    'voltage1' REAL NOT NULL,
+    'voltage2' REAL NOT NULL,
+    'voltage3' REAL NOT NULL,
+    'voltage4' REAL NOT NULL,
+    'voltage5' REAL NOT NULL,
+    'voltage6' REAL NOT NULL,
+    'voltage7' REAL NOT NULL,
+    'voltage8' REAL NOT NULL,
+    'voltage9' REAL NOT NULL,
+    'voltage10' REAL NOT NULL,
+    'voltage11' REAL NOT NULL,
+    'voltage12' REAL NOT NULL,
+    'temperature1' REAL NOT NULL,
+    'temperature2' REAL NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS 'bms2' (
+    'date' DATE NOT NULL,
+    'time' TIME NOT NULL,
+    'voltage1' REAL NOT NULL,
+    'voltage2' REAL NOT NULL,
+    'voltage3' REAL NOT NULL,
+    'voltage4' REAL NOT NULL,
+    'voltage5' REAL NOT NULL,
+    'voltage6' REAL NOT NULL,
+    'voltage7' REAL NOT NULL,
+    'voltage8' REAL NOT NULL,
+    'voltage9' REAL NOT NULL,
+    'voltage10' REAL NOT NULL,
+    'voltage11' REAL NOT NULL,
+    'voltage12' REAL NOT NULL,
+    'temperature1' REAL NOT NULL,
+    'temperature2' REAL NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS 'bms3' (
     'date' DATE NOT NULL,
     'time' TIME NOT NULL,
     'voltage1' REAL NOT NULL,
@@ -17,45 +55,7 @@ CREATE TABLE IF NOT EXISTS bms1 (
     'temperature2' REAL NOT NULL,
 );
 
-CREATE TABLE IF NOT EXISTS bms2 (
-    'date' DATE NOT NULL,
-    'time' TIME NOT NULL,
-    'voltage1' REAL NOT NULL,
-    'voltage2' REAL NOT NULL,
-    'voltage3' REAL NOT NULL,
-    'voltage4' REAL NOT NULL,
-    'voltage5' REAL NOT NULL,
-    'voltage6' REAL NOT NULL,
-    'voltage7' REAL NOT NULL,
-    'voltage8' REAL NOT NULL,
-    'voltage9' REAL NOT NULL,
-    'voltage10' REAL NOT NULL,
-    'voltage11' REAL NOT NULL,
-    'voltage12' REAL NOT NULL,
-    'temperature1' REAL NOT NULL,
-    'temperature2' REAL NOT NULL,
-);
-
-CREATE TABLE IF NOT EXISTS bms3 (
-    'date' DATE NOT NULL,
-    'time' TIME NOT NULL,
-    'voltage1' REAL NOT NULL,
-    'voltage2' REAL NOT NULL,
-    'voltage3' REAL NOT NULL,
-    'voltage4' REAL NOT NULL,
-    'voltage5' REAL NOT NULL,
-    'voltage6' REAL NOT NULL,
-    'voltage7' REAL NOT NULL,
-    'voltage8' REAL NOT NULL,
-    'voltage9' REAL NOT NULL,
-    'voltage10' REAL NOT NULL,
-    'voltage11' REAL NOT NULL,
-    'voltage12' REAL NOT NULL,
-    'temperature1' REAL NOT NULL,
-    'temperature2' REAL NOT NULL,
-);
-
-CREATE TABLE IF NOT EXISTS general(
+CREATE TABLE IF NOT EXISTS 'general' (
     'date' DATE NOT NULL,
     'time' TIME NOT NULL,
     'total_voltage' REAL,
